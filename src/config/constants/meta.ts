7df492dd0659b67a8eaa01176ba119d8fd4280c3
Misians <71899284+Misians@users.noreply.github.com>
2022-08-18 21:22:07 -0300

    correções

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -6,7 +6,7 @@ export const DEFAULT_META: PageMeta = {
   title: 'Jedals',
   description:
     'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust.',
-  image: 'https://pancakeswap.finance/images/hero.png',
+  image: 'https://i.imgur.com/h0Tq6xi.png',
 }
 
 interface PathList {
